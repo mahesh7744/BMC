942c07d2c5619c333b3a7773571c4798983728b8
aniketlokare-948 <lokareaniket1155@gmail.com>
2025-04-02 01:48:25 -0700

    membermaster

---
+++ lib/master/membermaster.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Membermaster extends StatefulWidget {
+  const Membermaster({super.key});
+
+  @override
+  State<Membermaster> createState() => _MembermasterState();
+}
+
+class _MembermasterState extends State<Membermaster> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
