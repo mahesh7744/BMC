f5a0ad712cbc7661aace9affd640becac21baf75
mahesh7744 <maheshasangi74@gmail.com>
2025-04-02 14:28:23 +0530

    main update

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,23 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-void main() {
-  runApp(const MyApp());
+import 'package:bmc/master/membermaster.dart';
+import 'package:flutter/material.dart';
+import 'package:window_manager/window_manager.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (Platform.isWindows) {
+    await windowManager.ensureInitialized();
+
+    windowManager.waitUntilReadyToShow().then((_) async {
+      await windowManager.center(); // मध्यभागी ठेवा
+      await windowManager.show(); // विंडो दाखवा
+      await windowManager.maximize();
+    });
+  }
+  runApp(
+    MyApp(),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -9,6 +25,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: "/login",
+      routes: {
+        "/membermaster": (context) => Membermaster(),
+      },
+    );
   }
 }
